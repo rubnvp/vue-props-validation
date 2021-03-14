5f4faed6e2d37dcdeaace56225c47d5b0c1e535e
Ruben <rubn.vp@gmail.com>
2021-03-14 16:30:16 +0100

    refactor code and semicolon consistence

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 export function objectValidator(options) {
     return (rawValues) => {
-        if (!isObject(rawValues)) return false;
+        if (!isObject(rawValues)) return false
         for (const key in options) {
         let opt = options[key]
         if (opt == null) continue
@@ -10,10 +10,10 @@ export function objectValidator(options) {
             opt = { type: opt, required: true }
         }
         if (!validateProp(key, rawValues[key], opt, !hasOwn(rawValues, key))) {
-            return false;
+            return false
         }
         }
-        return true;
+        return true
     }
 }
 
@@ -21,7 +21,7 @@ export function objectValidator(options) {
 
 export function arrayValidator(opt) {
     return (rawValues) => {
-        if (!isArray(rawValues)) return false;
+        if (!isArray(rawValues)) return false
         if (!isObject(opt) || isArray(opt)) {
         opt = { type: opt, required: true }
         }
@@ -31,31 +31,40 @@ export function arrayValidator(opt) {
 
 /** Code from vue-next repository: https://github.com/vuejs/vue-next/blob/1a955e22785cd3fea32b80aa58049c09bba4e321/packages/runtime-core/src/componentProps.ts#L476 */
 
-const hasOwnProperty = Object.prototype.hasOwnProperty
-const hasOwn = (val, key) => hasOwnProperty.call(val, key)
-
-// use function string name to check type constructors
-// so that it works across vms / iframes.
-function getType(ctor) {
-  const match = ctor && ctor.toString().match(/^\s*function (\w+)/)
-  return match ? match[1] : ''
-}
-
-function makeMap(str, expectsLowerCase) {
-  const map = Object.create(null)
-  const list = str.split(',')
-  for (let i = 0; i < list.length; i++) {
-    map[list[i]] = true
+function validateProp(name, value, prop, isAbsent) {
+  const { type, required, validator } = prop
+  // required!
+  if (required && isAbsent) {
+    //   console.warn('Missing required prop: "' + name + '"')
+    return false
   }
-  return expectsLowerCase ? val => !!map[val.toLowerCase()] : val => !!map[val]
+  // missing but optional
+  if (value == null && !prop.required) {
+    return true
+  }
+  // type check
+  if (type != null && type !== true) {
+    let isValid = false
+    const types = isArray(type) ? type : [type]
+    // const expectedTypes = []
+    // value is valid as long as one of the specified types match
+    for (let i = 0; i < types.length && !isValid; i++) {
+      const { valid, expectedType } = assertType(value, types[i])
+      // expectedTypes.push(expectedType || '')
+      isValid = valid
+    }
+    if (!isValid) {
+      // console.warn(getInvalidTypeMessage(name, value, expectedTypes))
+      return false
+    }
+  }
+  // custom validator
+  if (validator && !validator(value)) {
+    // warn('Invalid prop: custom validator check failed for prop "' + name + '".')
+    return false
+  }
+  return true
 }
-
-const isSimpleType = makeMap(
-  'String,Number,Boolean,Function,Symbol,BigInt'
-)
-
-const isObject = (val) => val !== null && typeof val === 'object'
-const isArray = Array.isArray
 
 function assertType(value, type) {
   let valid
@@ -80,37 +89,28 @@ function assertType(value, type) {
   }
 }
 
-function validateProp(name, value, prop, isAbsent) {
-  const { type, required, validator } = prop
-  // required!
-  if (required && isAbsent) {
-    //   console.warn('Missing required prop: "' + name + '"')
-    return false;
-  }
-  // missing but optional
-  if (value == null && !prop.required) {
-    return true;
-  }
-  // type check
-  if (type != null && type !== true) {
-    let isValid = false
-    const types = isArray(type) ? type : [type]
-    // const expectedTypes = []
-    // value is valid as long as one of the specified types match
-    for (let i = 0; i < types.length && !isValid; i++) {
-      const { valid, expectedType } = assertType(value, types[i])
-      // expectedTypes.push(expectedType || '')
-      isValid = valid
-    }
-    if (!isValid) {
-      // console.warn(getInvalidTypeMessage(name, value, expectedTypes))
-      return false;
-    }
-  }
-  // custom validator
-  if (validator && !validator(value)) {
-    // warn('Invalid prop: custom validator check failed for prop "' + name + '".')
-    return false;
-  }
-  return true;
+// use function string name to check type constructors
+// so that it works across vms / iframes.
+function getType(ctor) {
+  const match = ctor && ctor.toString().match(/^\s*function (\w+)/)
+  return match ? match[1] : ''
 }
+
+const isSimpleType = makeMap(
+  'String,Number,Boolean,Function,Symbol,BigInt'
+)
+
+function makeMap(str, expectsLowerCase) {
+  const map = Object.create(null)
+  const list = str.split(',')
+  for (let i = 0; i < list.length; i++) {
+    map[list[i]] = true
+  }
+  return expectsLowerCase ? val => !!map[val.toLowerCase()] : val => !!map[val]
+}
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwn = (val, key) => hasOwnProperty.call(val, key)
+
+const isObject = (val) => val !== null && typeof val === 'object'
+const isArray = Array.isArray
