28fdaf73774e39853e380c09555637f1a027adf5
Ruben <rubn.vp@gmail.com>
2021-03-17 11:48:00 +0100

    umd transform and ie 11 support

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  plugins: [
+    ['@babel/plugin-transform-modules-umd', {
+      exactGlobals: true,
+      globals: {
+        index: 'VueProps',
+      },
+    }],
+  ],
   presets: [
     [
       '@babel/preset-env',
       {
         targets: {
           node: 'current',
+          ie: '11',
         },
       },
     ],
